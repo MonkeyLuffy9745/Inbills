874db9d4d5b8db38b8fd13a8cdd0c576248850d5
MonkeyLuffy9745 <bilazajeff9@gmail.com>
2025-05-22 19:18:13 +0000

    first commit

---
+++ app/Models/Invoice.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Invoice extends Model
+{
+    protected $fillable = [
+        'invoice_number',
+        'invoice_date',
+        'invoice_amount',
+        'invoice_status',
+        'invoice_due_date',
+        'payement_method',
+        'payement_date',
+        'vat',
+        'client_id',
+        'user_id',
+    ];
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+    
+    
+}
---
+++ database/migrations/2025_05_22_185201_create_invoices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->string('invoice_number');
+            $table->date('invoice_date');
+            $table->decimal('invoice_amount', 10, 2);
+            $table->enum('invoice_status', ['pending', 'paid', 'overdue'])->default('pending');
+            $table->date('invoice_due_date');
+            $table->string('payement_method');
+            $table->date('payement_date');
+            $table->decimal('vat', 10, 2);
+            $table->foreignId('client_id')->constrained('clients')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
